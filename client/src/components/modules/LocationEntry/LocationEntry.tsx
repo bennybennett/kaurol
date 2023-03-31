248918b40eddb5c04b5898ec61d884035da5c7da
benny <mattbennybennett@gmail.com>
2023-03-30 22:02:50 -0400

    show all the sub-locations and whatnot. boy, i'm really good at this whole PrOgRaMmInG thing, aren't i? are there...like...awards for programming? because...and, i don't want to toot my own horn, but come on...that component is a MESS but it WORKS SO WELL and isn't that what programming is all about?

--- client/src/components/modules/LocationEntry/LocationEntry.tsx
+++ client/src/components/modules/LocationEntry/LocationEntry.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import styles from './LocationEntry.module.css';
 import { ILocation } from '../../../../../shared/types/entry';
 import { LocationsContext } from '../../contexts/LocationsContext/LocationsContext';
@@ -21,18 +21,85 @@ const LocationEntry: React.FC<LocationEntryProps> = ({ entry }) => {
     return obj && typeof obj.title === 'string';
   }
 
+  const generateLineage = (
+    loc: ILocation,
+    lineage: ILocation[] = []
+  ): ILocation[] => {
+    console.log(loc);
+    if (!isILocation(loc.parent)) {
+      return lineage;
+    }
+
+    const parent = locations.find(
+      (location) => isILocation(loc.parent) && location._id === loc.parent._id
+    );
+    console.log(locations);
+    if (!parent) {
+      return lineage;
+    }
+
+    lineage.unshift(parent);
+    return generateLineage(parent, lineage);
+  };
+
+  const lineage = generateLineage(location);
+
   return (
     <div className={styles.LocationEntry}>
-      <div>
+      <div className={styles['location-category']}>
         Category: <Chip>{entry.locationType}</Chip>
       </div>
-      {isILocation(location.parent) && (
-        <div>
-          Parent:{' '}
-          <Link href={`/entries/${location.parent._id}`}>
-            {location.parent.title}
-          </Link>
-        </div>
+      {(lineage.length > 0 || location.sublocations?.length) && (
+        <table className={styles['lineage-table']}>
+          {lineage.length > 0 &&
+            lineage.map((loc) => (
+              <tr key={loc._id}>
+                <td>
+                  <Link href={`/entries/${loc._id}`}>{loc.title}</Link>
+                </td>
+                <td>
+                  <small>{loc.title}'s Sub-Locations</small>
+                  <br />
+                  {loc.sublocations?.map((subloc, subIndex) => (
+                    <React.Fragment key={subloc._id}>
+                      {subIndex > 0 && <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>}
+                      {subloc._id === entry._id ? (
+                        <span className={styles['current-location']}>
+                          {subloc.title}
+                        </span>
+                      ) : (
+                        <Link href={`/entries/${subloc._id}`}>
+                          {subloc.title}
+                        </Link>
+                      )}
+                    </React.Fragment>
+                  ))}
+                </td>
+              </tr>
+            ))}
+          {location.sublocations && location.sublocations.length > 0 && (
+            <tr>
+              <td colSpan={2}>
+                <small>{location.title}'s Sub-Locations</small>
+                <br />
+                {location.sublocations?.map((subloc, subIndex) => (
+                  <React.Fragment key={subloc._id}>
+                    {subIndex > 0 && <span>&nbsp;&nbsp;|&nbsp;&nbsp;</span>}
+                    {subloc._id === entry._id ? (
+                      <span className={styles['current-location']}>
+                        {subloc.title}
+                      </span>
+                    ) : (
+                      <Link href={`/entries/${subloc._id}`}>
+                        {subloc.title}
+                      </Link>
+                    )}
+                  </React.Fragment>
+                ))}
+              </td>
+            </tr>
+          )}
+        </table>
       )}
     </div>
   );
