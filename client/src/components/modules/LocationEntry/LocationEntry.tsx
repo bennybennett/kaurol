67b58808047a947f4e492adb162d0947d3443140
benny <mattbennybennett@gmail.com>
2023-03-30 21:09:06 -0400

    show parent locations, with some Chip'ifying™️

--- client/src/components/modules/LocationEntry/LocationEntry.tsx
+++ client/src/components/modules/LocationEntry/LocationEntry.tsx
@@ -1,7 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from './LocationEntry.module.css';
 import { ILocation } from '../../../../../shared/types/entry';
 import { LocationsContext } from '../../contexts/LocationsContext/LocationsContext';
+import Chip from '../../ui/Chip/Chip';
+import Link from '../../shared/Link/Link';
 
 interface LocationEntryProps {
   entry: ILocation;
@@ -9,12 +11,29 @@ interface LocationEntryProps {
 
 const LocationEntry: React.FC<LocationEntryProps> = ({ entry }) => {
   const locations = useContext(LocationsContext);
+  const location = locations.find((location) => location._id === entry._id);
 
-  console.log(locations);
+  if (!location) {
+    return <h1>Location not found!</h1>;
+  }
+
+  function isILocation(obj: any): obj is ILocation {
+    return obj && typeof obj.title === 'string';
+  }
 
   return (
     <div className={styles.LocationEntry}>
-      <h4>Category: {entry.locationType}</h4>
+      <div>
+        Category: <Chip>{entry.locationType}</Chip>
+      </div>
+      {isILocation(location.parent) && (
+        <div>
+          Parent:{' '}
+          <Link href={`/entries/${location.parent._id}`}>
+            {location.parent.title}
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
