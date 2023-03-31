37023774e8cc0b9b848554258bdac490428f7f2d
benny <mattbennybennett@gmail.com>
2023-03-30 21:03:56 -0400

    fnv1a babyyyyyy

--- client/src/util/color.tsx
+++ client/src/util/color.tsx
@@ -1,9 +1,20 @@
-export const generateColor = (text: string) => {
-  const hash = text
-    .split('')
-    .reduce((acc, char) => (acc * 56 + char.charCodeAt(0)) % 256, 0);
+function fnv1a(text: string) {
+  const FNV_PRIME = 0x01000193;
+  const FNV_OFFSET_BASIS = 0x811c9dc5;
+  let hash = FNV_OFFSET_BASIS;
 
-  const hue = Math.floor((hash / 256) * 360);
+  for (const char of text) {
+    hash ^= char.charCodeAt(0);
+    hash *= FNV_PRIME;
+  }
+
+  return hash >>> 0;
+}
+
+export const generateColor = (text: string) => {
+  const hash = fnv1a(text);
+
+  const hue = Math.floor((hash / 0xffffffff) * 360);
 
   const color = `hsl(${hue}, 50%, 50%)`;
   const darkerColor = `hsl(${hue}, 60%, 20%)`;
